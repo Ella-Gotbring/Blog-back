71bdaaacff9b513d0fe02aa3928b6b94141255d2
EllaGotbringg <54885982+EllaGotbringg@users.noreply.github.com>
2021-03-02 10:51:03 +0100

    started server

---
+++ app.js
@@ -0,0 +1,9 @@
+const http = require('http');
+
+let app = http.createServer((req,res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello world!');
+});
+
+app.listen(3000, '127.0.0.1');
+console.log('Node server started');
